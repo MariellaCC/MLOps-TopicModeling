3e293b47048616bb8b462e54d933e60978cb683d
Mariella CC <MariellaCC@users.noreply.github.com>
2023-06-12 15:11:31 +0200

    style: variable names

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,11 @@
 DROP DATABASE IF EXISTS `DB`;
 CREATE DATABASE `DB`;
 USE `DB`;
-DROP TABLE IF EXISTS `Journals`;
-CREATE TABLE `Journals` (
-  `FileName` varchar(255) NOT NULL,
-  `TextContent` text,
-  `TextDate` varchar(10),
-  `PubRef` varchar(255)
+DROP TABLE IF EXISTS `sources`;
+CREATE TABLE `sources` (
+  `file_name` varchar(255) NOT NULL,
+  `file_content` text,
+  `date` varchar(10),
+  `publication_name` varchar(255),
+  `publication_ref` varchar(255)
 );
