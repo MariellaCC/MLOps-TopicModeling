bd498271e0871af2d586dd001f58b12d63d00553
Mariella CC <MariellaCC@users.noreply.github.com>
2023-06-25 23:55:47 +0200

    add tables for train test and metrics

--- db/schema.sql
+++ db/schema.sql
@@ -2,10 +2,22 @@ DROP DATABASE IF EXISTS `DB`;
 CREATE DATABASE `DB`;
 USE `DB`;
 DROP TABLE IF EXISTS `sources`;
-CREATE TABLE `sources` (
+CREATE TABLE `sources_train` (
   `file_name` varchar(255) NOT NULL,
   `file_content` text,
   `date` varchar(10),
   `publication_name` varchar(255),
   `publication_ref` varchar(255)
+);
+CREATE TABLE `sources_test` (
+  `file_name` varchar(255) NOT NULL,
+  `file_content` text,
+  `date` varchar(10),
+  `publication_name` varchar(255),
+  `publication_ref` varchar(255)
+);
+CREATE TABLE `metrics` (
+  `timestamp` varchar(255) NOT NULL,
+  `coherence` decimal,
+  `perplexity` decimal
 );
