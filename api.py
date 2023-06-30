500ba1ea355a85e9743e937c4d419cb37b634188
Mariella CC <MariellaCC@users.noreply.github.com>
2023-06-30 04:01:54 +0200

    feat: wip retrain route

--- api.py
+++ api.py
@@ -246,3 +246,21 @@ def metrics_new_texts(n: Annotated[int, Path(description="Enter number of texts.
            'coherence': coherence,
            'alert': alert}
     return dic
+
+@app.get('/doc/retrain_model/{nr_topic_min}/{nr_topic_max}/{n}')
+def retrain_model(nr_topic_min: Annotated[int, Path(description="Enter min number of topics.")],nr_topic_max: Annotated[int, Path(description="Enter max number of topics.")],n: Annotated[int, Path(description="Enter number of texts.")]):
+    query = text(f"select * from sources LIMIT {n}")
+    with engine.connect() as conn:
+        result = conn.execute(query)
+    lis=[]
+    print('hello')
+    print(len(lis))
+    for row in result:
+        lis.append(row[5])
+    
+    res_df = api_modules.retrain_model(lis, [nr_topic_min,nr_topic_max])
+
+    return res_df.to_dict(orient="records")
+
+
+
--- api_modules/__init__.py
+++ api_modules/__init__.py
@@ -232,3 +232,61 @@ def compute_metrics(texts,lda_model,id2word,threshold_coherence=0.38,threshold_p
     alert = "Retrain needed" if (coherence < threshold_coherence or perplexity > threshold_perplexity) else None
 
     return lda_model.print_topics(num_words=10), perplexity, coherence, alert
+
+
+def retrain_model(texts, topic_nr):
+    """
+    Retrain model and compute metrics for range of topic numbers.
+
+    Args:
+        texts (list): List containing the texts to train the model.
+        topic_nr (list): List containing min and max number of topics to test.
+
+    Returns:
+        error (str): Error message.
+
+    """
+
+    # Create df from new texts
+    corpus_df = pd.DataFrame(texts,columns=['file_content'])
+
+    # Tokenize documents
+    corpus_df = tokenize_documents(corpus_df, 'file_content')
+
+    # Preprocess tokens
+    corpus_df = preprocess_tokens(corpus_df, 'tokens')
+
+    # Load stopwords
+    stopwords_list = load_stopwords('stop_words.csv')
+
+    # Remove stopwords
+    corpus_df = remove_stopwords(corpus_df, 'doc_prep', stopwords_list)
+
+    # Create bigrams
+    corpus_df = create_bigrams(corpus_df, 'doc_prep_nostop')
+
+    id2word = get_id2word(corpus_df)
+
+    corpus = get_bow(id2word,corpus_df['bigrams'])
+
+    topics_nr = []
+    coherence_values_gensim = []
+    perplexitys = []
+    models = []
+    models_idx = [x for x in range(topic_nr[0],topic_nr[1])]
+    for num_topics in range(topic_nr[0],topic_nr[1]):
+        model = LdaModel(corpus, id2word=id2word, num_topics=num_topics, eval_every = None, chunksize=100,
+                                          passes=10,random_state=100)
+        models.append(model)
+        coherencemodel = CoherenceModel(model=model, texts=corpus_df, dictionary=id2word, coherence='c_v')
+        coherence_value = coherencemodel.get_coherence()
+        perplexity = model.log_perplexity(corpus)
+        perplexitys.append(perplexity)
+        coherence_values_gensim.append(coherence_value)
+        topics_nr.append(str(num_topics))
+
+    df_coherence = pd.DataFrame(topics_nr, columns=['Number of topics'])
+    df_coherence['Coherence'] = coherence_values_gensim
+    df_coherence['Perplexity'] = perplexitys
+
+    return df_coherence
